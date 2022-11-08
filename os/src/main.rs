9c1a47973b673587623c7538529995ad0871867b
A0α无∞非Ω9Z <1248813420@qq.com>
2022-11-08 11:10:44 +0800

    3. 配置栈空间布局 4. 清空bss段 5. 实现裸机打印输出信息

--- os/src/main.rs
+++ os/src/main.rs
@@ -70,3 +70,20 @@ macro_rules! println {
         print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
 }
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+use core::arch::global_asm;
+
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    loop{};
+}
