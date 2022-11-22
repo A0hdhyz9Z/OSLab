1d762d4e1e62123401b84673b9cda0116806cbce
A0α无∞非Ω9Z <1248813420@qq.com>
2022-11-22 10:30:48 +0800

    1. 实现应用程序

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![feature(linkage)]
 #![feature(panic_info_message)]
+
 #[macro_use]
 pub mod console;
 mod syscall;
 mod lang_items;
+
 fn clear_bss() {
     extern "C" {
         fn start_bss();
@@ -14,6 +16,7 @@ fn clear_bss() {
         unsafe { (addr as *mut u8).write_volatile(0); }
     });
 }
+
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
@@ -21,11 +24,16 @@ pub extern "C" fn _start() -> ! {
     exit(main());
     panic!("unreachable after sys_exit!");
 }
+
 #[linkage = "weak"]
 #[no_mangle]
 fn main() -> i32 {
     panic!("Cannot find main!");
 }
+
+
 use syscall::*;
+
 pub fn write(fd: usize, buf: &[u8]) -> isize { sys_write(fd, buf) }
 pub fn exit(exit_code: i32) -> isize { sys_exit(exit_code) }
+pub fn yield_() -> isize { sys_yield() }
